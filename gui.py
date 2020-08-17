9df15a691a8494d34b15fb452f4353ffdb3ad5ac
imchini <chinidev27@gmail.com>
2020-08-17 10:12:58 -0700

    started working on button logic

--- gui.py
+++ gui.py
@@ -5,10 +5,13 @@ import random
 
 insult=("you "+meanMachine.list1[random.randint(0,18)]+", "+meanMachine.list2[random.randint(0,18)]+", "+meanMachine.list3[random.randint(0,26)]+"!")
 
+def showText():
+  insult
+
 root=Tk()
 root.title("Get Insulted!")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
-ttk.Button(root, text=insult).grid()
+ttk.Button(root, text="Generate Insult", command=showText()).grid()
 root.mainloop()
 
