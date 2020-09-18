aefea3a439ed85fd4f463131c7d70f4c8ad0c54c
imchini <chinidev27@gmail.com>
2020-09-17 17:53:47 -0700

    generating insults on click and population works

--- gui.py
+++ gui.py
@@ -3,18 +3,21 @@ from tkinter import *
 from tkinter import ttk
 import random
 
-insult=("you "+meanMachine.list1[random.randint(0,18)]+", "+meanMachine.list2[random.randint(0,18)]+", "+meanMachine.list3[random.randint(0,26)]+"!")
+def genInsult():
+  label.configure(text= ("you "+meanMachine.list1[random.randint(0,18)]+", "+meanMachine.list2[random.randint(0,18)]+", "+meanMachine.list3[random.randint(0,26)]+"!"))
 
 def showText():
-  insult
+  print(genInsult())
 
 root=Tk()
 root.title("Get Insulted!")
-root.geometry("400x400")
+root.geometry("800x400")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 ttk.Button(root,text="Generate Insult",
-                command=showText).grid()
+                command=genInsult).grid()
+label = ttk.Label(root)
+label.grid(column = 0, row = 0, pady = 2)
 ttk.Button(root,
                 text="Quit",
                 command=root.destroy).grid()
